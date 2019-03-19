8d80a6f47754aff7ee3710b8eb14ac5b0d79bd6a
Jae Bradley <jae.b.bradley@gmail.com>
2019-03-18 21:50:21 -0700

    Only inject component for books

--- source/content/index.jsx
+++ source/content/index.jsx
@@ -5,20 +5,32 @@ import domLoaded from 'dom-loaded';
 
 import App from './App';
 
+const VALID_CATEGORIES = [
+  'books',
+  'digital-text',
+];
+
 async function inject() {
   await domLoaded;
-  // Kindle Tab
-  let element = document.getElementById('reviewFeatureGroup');
-  if (!element) {
-    // Hard Cover Tab
-    element = document.getElementById('averageCustomerReviews');
-  }
 
-  if (element) {
-    const appDiv = document.createElement('div');
-    appDiv.setAttribute('id', 'goodreads-extension');
-    element.parentNode.insertBefore(appDiv, element.nextSibling);
-    ReactDOM.render(<App />, appDiv);
+  // Check category DOM element and ensure it's book or kindle category
+  const categoryElement = document.getElementById('nav-subnav');
+  if (categoryElement) {
+    if (VALID_CATEGORIES.includes(categoryElement.getAttribute('data-category'))) {
+      // Kindle Tab
+      let element = document.getElementById('reviewFeatureGroup');
+      if (!element) {
+        // Hard Cover Tab
+        element = document.getElementById('averageCustomerReviews');
+      }
+
+      if (element) {
+        const appDiv = document.createElement('div');
+        appDiv.setAttribute('id', 'goodreads-extension');
+        element.parentNode.insertBefore(appDiv, element.nextSibling);
+        ReactDOM.render(<App />, appDiv);
+      }
+    }
   }
 }
 try {
